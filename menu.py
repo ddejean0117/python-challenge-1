faca79f1501b50ec124dc03cc30fc7f78a912f86
ddejean0117 <djdejean@gmail.com>
2024-06-13 05:55:19 -0700

    third commit just testing

--- menu.py
+++ menu.py
@@ -130,7 +130,7 @@ while place_order:
                 print(f'customer input is now an {type(customer_input)}')
 
                 # 4. Check if the menu selection is in the menu items
-                
+                #commit test
                 #print(f'{int(menu_category_name)}')
                 
                 # #if customer_input in len(menu_category_name.keys):
